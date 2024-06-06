52f5c00b32008084e6dfb0e60b94586769d03d4a
unknown <jfalconpriv@gmail.com>
2024-06-05 21:15:43 -0500

    added light/dark mode styling

---
+++ Assets/js/form.js
@@ -0,0 +1,42 @@
+// Path: Assets/js/form.js
+
+// this code is to toggle the light/dark mode functionality of the website
+// this uses the document.getElementById method to get the element with id="flexSwitchCheckDefault"
+// the addEventListener method is used to listen for the change event on the toggle switch at the top of the page
+document.getElementById('flexSwitchCheckDefault').addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        document.querySelector('header').classList.remove('light-mode');
+        document.querySelector('header').classList.add('dark-mode');
+        document.querySelectorAll('.circle').forEach(function(element) {
+            element.classList.remove('light-mode');
+            element.classList.add('dark-mode');
+        });
+        document.querySelectorAll('.custom-form-control').forEach(function(element) {
+            element.classList.remove('light-mode');
+            element.classList.add('dark-mode');
+        });
+        document.querySelectorAll('.btn').forEach(function(element) {
+            element.classList.remove('light-mode');
+            element.classList.add('dark-mode');
+        });
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        document.querySelector('header').classList.remove('dark-mode');
+        document.querySelector('header').classList.add('light-mode');
+        document.querySelectorAll('.circle').forEach(function(element) {
+            element.classList.remove('dark-mode');
+            element.classList.add('light-mode');
+        });
+        document.querySelectorAll('.custom-form-control').forEach(function(element) {
+            element.classList.remove('dark-mode');
+            element.classList.add('light-mode');
+        });
+        document.querySelectorAll('.btn').forEach(function(element) {
+            element.classList.remove('dark-mode');
+            element.classList.add('light-mode');
+        });
+    }
+});
--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,3 +1,5 @@
+// Path: Assets/js/logic.js
+
 // this code is to redirect the user to the blog.html page
 // this uses the window.location.assign method to redirect the user to the blog.html page
 // given the button with id="postYourBlog" is pressed
