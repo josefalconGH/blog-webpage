f87d9a575ab6bfa3f667244b7070c06eb265f1fe
unknown <jfalconpriv@gmail.com>
2024-06-05 20:04:09 -0500

    added functionality to logic.js

---
+++ Assets/js/logic.js
@@ -0,0 +1,6 @@
+// this code is to redirect the user to the blog.html page
+// this uses the window.location.assign method to redirect the user to the blog.html page
+// given the button with id="postYourBlog" is pressed
+document.getElementById('postYourBlog').addEventListener('click', function() {
+window.location.assign('blog.html');
+});
